b989ca39406db847249cd211bb9bc5007974fde8
Drew Hess <src@drewhess.com>
2011-12-30 20:30:07 -1000

    Slightly better version of event details parser.

--- event-details.hs
+++ event-details.hs
@@ -7,21 +7,59 @@ import qualified Data.Map as Map
 -- namespace used by SXSW schedule documents
 ns = Just "http://www.w3.org/1999/xhtml"
 
-eventDetails url = do
-  --xml <- liftM (fromJust . parseXMLDoc) $ unsafeCurlGetString url
-  xml <- liftM (fromJust . parseXMLDoc) $ readFile "casiokids.html"
-  let artist = strContent $ findClass "event_name" xml
-      genre = strContent $ findClass "event_sub_category" xml
-      origin = strContent $ findClass "event_citystate" xml
-      img = theSrc $ findImg $ findClass "video_embed" xml
-      description = strContent $ findClass "main_content_desc" xml
-      date = strContent $ findClass "date" xml
-      time = strContent $ findClass "time" xml
-      venue =  strContent . findLink . head $ findClasses "venue" xml
-      age = strContent $ findClasses "venue" xml !! 1
-      addr = strContent $ findClass "address" xml
-      homepage = theHref . findLink $ findClass "web" xml
-  return $ Map.fromList [
+-- Parse event XML documents
+getEventDoc url = fmap parseXMLDoc $ readFile "casiokids.html"
+
+-- Extract event details
+byClass className el = (findAttr classAttr el) == Just className
+  where classAttr = unqual "class"
+
+findClass className = filterElement (byClass className)
+findClasses className = filterElements (byClass className)
+
+findImg = findElement imgEl
+  where imgEl = QName "img" ns Nothing
+
+findLink = findElement linkEl
+  where linkEl = QName "a" ns Nothing
+
+theSrc = findAttr srcAttr
+  where srcAttr = unqual "src"
+
+theHref = findAttr hrefAttr
+  where hrefAttr = unqual "href"
+    
+maybeStrContent :: Maybe Element -> Maybe String
+maybeStrContent = fmap strContent
+
+theArtist = maybeStrContent . findClass "event_name"
+theGenre =  maybeStrContent . findClass "event_sub_category"
+theOrigin = maybeStrContent . findClass "event_citystate"
+theArtistImg = theSrc <=< findImg <=< findClass "video_embed"
+theDescription = maybeStrContent . findClass "main_content_desc"
+theDate = maybeStrContent . findClass "date"
+theTime = maybeStrContent . findClass "time"
+theVenue = maybeStrContent . findLink <=< listToMaybe . findClasses "venue"
+theAddress = maybeStrContent . findClass "address"  
+theArtistHomepage = theHref <=< findLink <=< findClass "web"
+
+theAges = maybeStrContent . secondEl . findClasses "venue"
+  where secondEl (_:x:xs) = Just x
+        secondEl _ = Nothing
+  
+eventDetails xml =
+  let artist = theArtist xml
+      genre = theGenre xml
+      origin = theOrigin xml
+      img = theArtistImg xml
+      description = theDescription xml
+      date = theDate xml
+      time = theTime xml
+      venue = theVenue xml
+      ages = theAges xml
+      addr = theAddress xml
+      homepage = theArtistHomepage xml
+  in Map.fromList [
     ("artist", artist),
     ("genre", genre),
     ("origin", origin),
@@ -29,26 +67,14 @@ eventDetails url = do
     ("date", date),
     ("time", time),
     ("venue", venue),
-    ("age", age),
+    ("ages", ages),
     ("addr", addr),
     ("description", description),
     ("homepage", homepage)
     ]
-  where findClass name doc = fromJust $ filterElement (byClass name) doc
-        findClasses name doc = filterElements (byClass name) doc
-        findImg = fromJust . findElement imgEl
-        theSrc = fromJust . findAttr srcAttr
-        byClass name el = (findAttr classAttr el) == Just name
-        theHref = fromJust . findAttr hrefAttr
-        hrefAttr = unqual "href"
-        findLink = fromJust . findElement linkEl
-        linkEl = QName "a" ns Nothing
-        imgEl = QName "img" ns Nothing
-        classAttr = unqual "class"
-        srcAttr = unqual "src"
   
 main = do
-  mapRep <- liftM Map.showTree $ eventDetails "http://schedule.sxsw.com/events/event_MS14879"
-  putStr mapRep
+  xml <- getEventDoc "http://schedule.sxsw.com/events/event_MS14879"
+  putStr $ Map.showTree $ eventDetails $ fromJust xml
 
 unsafeCurlGetString url = curlGetString url [] >>= return . snd
