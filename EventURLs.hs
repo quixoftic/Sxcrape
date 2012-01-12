0b960967faaedf883bed0bca8504ede06ad1baf7
Drew Hess <src@drewhess.com>
2012-01-12 14:50:40 -0800

    Rename scheduleForDay -> scheduleURLForDay.

--- EventURLs.hs
+++ EventURLs.hs
@@ -8,12 +8,12 @@ import ParserUtils
 
 eventURLsForDay :: Num a => a -> IO [String]
 eventURLsForDay day = do
-  (_, html) <- curlGetString (scheduleForDay day) []
+  (_, html) <- curlGetString (scheduleURLForDay day) []
   let eventEls = findClasses "link_itemMusic" . fromMaybe blank_element $ parseXMLDoc html
   return $ map (makeAbsoluteURLFrom . fromJust . theHref) eventEls
 
-scheduleForDay :: Num a => a -> String
-scheduleForDay day = "http://schedule.sxsw.com/?conference=music&day=" ++ (show day) ++ "&category=Showcase#"
+scheduleURLForDay :: Num a => a -> String
+scheduleURLForDay day = "http://schedule.sxsw.com/?conference=music&day=" ++ (show day) ++ "&category=Showcase#"
 
 makeAbsoluteURLFrom :: String -> String
 makeAbsoluteURLFrom u = "http://schedule.sxsw.com" ++ u
