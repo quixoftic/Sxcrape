bad7e7c15dd82877d248f46abe35ce53d7a486d7
Drew Hess <src@drewhess.com>
2012-01-11 22:59:51 -0800

    GetEventDetails.hs: actually fetch URL, rather than reading from file.

--- GetEventDetails.hs
+++ GetEventDetails.hs
@@ -4,11 +4,13 @@ import Text.XML.Light
 import qualified Data.Map as Map
 import Data.Maybe
 
--- Parse event XML documents
-getEventDoc url = fmap parseXMLDoc $ readFile "casiokids.html"
-
+main :: IO ()
 main = do
   xml <- getEventDoc "http://schedule.sxsw.com/events/event_MS14879"
   putStr $ Map.showTree $ eventDetails $ fromJust xml
 
+getEventDoc :: String -> IO (Maybe Element)
+getEventDoc = fmap parseXMLDoc . unsafeCurlGetString
+
+unsafeCurlGetString :: String -> IO String
 unsafeCurlGetString url = curlGetString url [] >>= return . snd
