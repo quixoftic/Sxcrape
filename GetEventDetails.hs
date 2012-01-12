64047e1112c90af660c0f290c21d470d49c62f7c
Drew Hess <src@drewhess.com>
2012-01-11 23:09:59 -0800

    Slightly better formatting for GetEventDetails output.

--- GetEventDetails.hs
+++ GetEventDetails.hs
@@ -4,10 +4,13 @@ import Text.XML.Light
 import qualified Data.Map as Map
 import Data.Maybe
 
+eventURL = "http://schedule.sxsw.com/events/event_MS14879"
+
 main :: IO ()
 main = do
-  xml <- getEventDoc "http://schedule.sxsw.com/events/event_MS14879"
-  putStr $ Map.showTree $ eventDetails $ fromJust xml
+  Just xml <- getEventDoc eventURL
+  putStrLn $ "Event details for " ++ eventURL ++ ":\n"
+  mapM_ (\(k, v) -> putStrLn $ k ++ ": " ++ v) $ Map.toList $ eventDetails xml
 
 getEventDoc :: String -> IO (Maybe Element)
 getEventDoc = fmap parseXMLDoc . unsafeCurlGetString
