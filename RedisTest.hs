c08be900319843c6f0be9f8d4e4ddaefe5ae7708
Drew Hess <src@drewhess.com>
2012-02-06 16:46:06 -0800

    Refactor RedisTest.hs for better re-use.

--- RedisTest.hs
+++ RedisTest.hs
@@ -14,42 +14,36 @@ instance BS T.Text where
 (<:>) :: T.Text -> T.Text -> T.Text
 s1 <:> s2 = T.concat [s1, ":", s2]
 
-eventID :: T.Text
-eventID = "event.id"
+eventIDPrefix :: T.Text
+eventIDPrefix = "event.id"
 
-nextEventID :: T.Text
-nextEventID = "next.event.id"
+nextEventIDKey :: T.Text
+nextEventIDKey = "next.event.id"
 
-newEventID :: Redis -> IO (Int)
-newEventID r = incr r nextEventID >>= fromRInt
-    
 eventIDKey :: T.Text -> T.Text
-eventIDKey nativeEventID = eventID <:> nativeEventID
+eventIDKey nativeEventID = eventIDPrefix <:> nativeEventID
 
-getEventID :: T.Text -> Redis -> IO (Maybe Int)
-getEventID nativeEventID r = get r (eventIDKey nativeEventID) >>= fromRBulk
-
-setNxEventID :: T.Text -> Int -> Redis -> IO (Int)
-setNxEventID nativeEventID id r = setNx r (eventIDKey nativeEventID) id >>= fromRInt
-
--- Return the event ID if it exists, otherwise make a new one. This
--- function is race-free. If two processes call it at the same time,
--- only one will create a new event ID; the other will return the same
--- event ID as the first.
 getOrSetEventID :: T.Text -> Redis -> IO (Int)
-getOrSetEventID nativeEventID r = do
-  id <- getEventID nativeEventID r
+getOrSetEventID nativeEventID r = getOrSetID (eventIDKey nativeEventID) nextEventIDKey r
+
+-- Return an ID for idKey if it exists, otherwise make a new one. This
+-- function is race-free. If two processes call it at the same time,
+-- only one will create a new ID; the other will return the same ID as
+-- the first.
+getOrSetID :: T.Text -> T.Text -> Redis ->IO (Int)
+getOrSetID idKey nextIdKey r = do
+  id <- get r idKey >>= fromRBulk
   case id of
     Just theID -> return theID
     Nothing -> do
-      newID <- newEventID r
-      reply <- setNxEventID nativeEventID newID r
+      newID <- incr r nextIdKey >>= fromRInt
+      reply <- setNx r idKey newID >>= fromRInt
       if (reply == 1)
         then return newID
-        else liftM fromJust $ getEventID nativeEventID r 
+        else liftM fromJust $ get r idKey >>= fromRBulk
 
 insertKey :: Redis -> IO (Reply Int)
 insertKey r = incr r ("test:key" :: T.Text)
 
 main :: IO (Int)
-main = connect localhost defaultPort >>= getOrSetEventID "MS1001"
+main = connect localhost defaultPort >>= getOrSetEventID "MS1002"
