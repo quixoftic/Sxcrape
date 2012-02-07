8c86bf5a622dcdb98e0cd77e3690fc35a0b66f56
Drew Hess <src@drewhess.com>
2012-02-07 02:34:18 -0800

    Much improved type safety for Redis keys.
    ID keys are now a different type class than "next ID" keys, preventing
    a key transpose in function arguments.

--- RedisTest.hs
+++ RedisTest.hs
@@ -11,34 +11,50 @@ import qualified Data.ByteString.Char8 as B
 
 -- Generic BS T.Text instance for converting all the *Key types to
 -- Redis BS (as long as they derive from BS).
+--
 instance BS T.Text where
   toBS = E.encodeUtf8
   fromBS = E.decodeUtf8
-  
+
+-- All x.id:xxx Redis keys should derive from this type class.
+class IDKey a where
+  idKeyToText :: a -> T.Text
+
+-- All next.x.id Redis keys should derive from this type class.
+class NextIDKey a where
+  nextIDKeyToText :: a -> T.Text
+
+-- All key types should wrap this newtype, rather than T.Text. It
+-- prevents Haskell from automatically converting T.Text arguments to
+-- IDKey or NextIDKey when applying getOrSetID.
+--
+newtype Key = Key { getKey :: T.Text } deriving (Eq, Show)
+
+instance IDKey Key where
+  idKeyToText = getKey
+
+instance NextIDKey Key where
+  nextIDKeyToText = getKey
+
+-- Build keys from namespaces with this operator.
 (<:>) :: T.Text -> T.Text -> T.Text
 s1 <:> s2 = T.concat [s1, ":", s2]
 
-newtype IDKey = IDKey { getIDKey :: T.Text } deriving (Eq, Show, BS)
-newtype NextIDKey = NextIDKey { getNextIDKey :: T.Text } deriving (Eq, Show, BS)
-
-newtype NextEventIDKey = NextEventIDKey { getNextEventIDKey :: NextIDKey } deriving (Eq, Show)
+-- Event keys.
+newtype EventIDKey = EventIDKey { getEventIDKey :: Key } deriving (Eq, Show, IDKey)
+newtype NextEventIDKey = NextEventIDKey { getNextEventIDKey :: Key } deriving (Eq, Show, NextIDKey)
 
 nextEventIDKey :: NextEventIDKey
-nextEventIDKey = NextEventIDKey $ NextIDKey "next.event.id"
-
-newtype EventIDKey = EventIDKey { getEventIDKey :: IDKey } deriving (Eq, Show)
+nextEventIDKey = NextEventIDKey $ Key "next.event.id"
 
 eventIDKeyPrefix :: T.Text
 eventIDKeyPrefix = "event.id"
 
 eventIDKey :: T.Text -> EventIDKey
-eventIDKey nativeEventID = EventIDKey $ IDKey $ eventIDKeyPrefix <:> nativeEventID
+eventIDKey nativeEventID = EventIDKey $ Key $ eventIDKeyPrefix <:> nativeEventID
 
 getOrSetEventID :: T.Text -> Redis -> IO (Int)
-getOrSetEventID nativeEventID r = getOrSetID (getEventIDKey (eventIDKey nativeEventID)) (getNextEventIDKey nextEventIDKey) r
-
--- getOrSetFooID :: T.Text -> T.Text -> Redis -> IO (Int)
--- getOrSetFooID x y r = getOrSetID x y r
+getOrSetEventID nativeEventID r = getOrSetID (eventIDKey nativeEventID) nextEventIDKey r
 
 -- artistIDKeyPrefix :: T.Text
 -- artistIDKeyPrefix = "artist.id"
@@ -64,24 +80,27 @@ getOrSetEventID nativeEventID r = getOrSetID (getEventIDKey (eventIDKey nativeEv
 -- getOrSetVenueID :: T.Text -> Redis -> IO (Int)
 -- getOrSetVenueID nativeVenueID r = getOrSetID (venueIDKey nativeVenueID) nextVenueIDKey r
 
--- Return an ID for idKey if it exists, otherwise make a new one. This
--- function is race-free. If two processes call it at the same time,
--- only one will create a new ID; the other will return the same ID as
--- the first.
-getOrSetID :: IDKey -> NextIDKey -> Redis ->IO (Int)
+-- Return an ID for idKey if it exists, otherwise make a new one by
+-- incrementing the next ID key. This function is race-free. If two
+-- processes call it at the same time, only one will create a new ID;
+-- the other will return the same ID as the first.
+--
+getOrSetID :: (IDKey a, NextIDKey b) => a -> b -> Redis ->IO (Int)
 getOrSetID idKey nextIdKey r = do
-  id <- get r idKey >>= fromRBulk
+  id <- get r (idKeyToText idKey) >>= fromRBulk
   case id of
     Just theID -> return theID
     Nothing -> do
-      newID <- incr r nextIdKey >>= fromRInt
-      reply <- setNx r idKey newID >>= fromRInt
+      newID <- incr r (nextIDKeyToText nextIdKey) >>= fromRInt
+      reply <- setNx r (idKeyToText idKey) newID >>= fromRInt
       if (reply == 1)
         then return newID
-        else liftM fromJust $ get r idKey >>= fromRBulk
-
-insertKey :: Redis -> IO (Reply Int)
-insertKey r = incr r ("test:key" :: T.Text)
+        else liftM fromJust $ get r (idKeyToText idKey) >>= fromRBulk
 
 main :: IO (Int)
-main = connect localhost defaultPort >>= getOrSetEventID "MS1003"
+main = connect localhost defaultPort >>= getOrSetEventID "MS1005"
+
+-- Canary test for key type-safety. This should not compile:
+--
+-- getOrSetFooID :: T.Text -> T.Text -> Redis -> IO (Int)
+-- getOrSetFooID x y r = getOrSetID x y r
