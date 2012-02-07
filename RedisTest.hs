7b7f45e694197f0483cc1792c267bb23fec6b2e5
Drew Hess <src@drewhess.com>
2012-02-06 23:13:11 -0800

    Experimenting with types for Redis keys.
    In the Haskell Redis bindings, keys are always of type BS (a Redis
    ByteString class), which means there's no type safety for keys --
    they're basically all just text.
    
    Still working out the details, but if successful, this branch will
    provide type safety for key types (e.g., so I don't use "event.id" for
    a key when I mean "next.event.id").

--- RedisTest.hs
+++ RedisTest.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving, DeriveDataTypeable #-}
 
 import Database.Redis.Redis
 import Database.Redis.ByteStringClass (BS, toBS, fromBS)
@@ -6,55 +6,68 @@ import Data.Maybe
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
 
+-- Generic BS T.Text instance for converting all the *Key types to
+-- Redis BS (as long as they derive from BS).
 instance BS T.Text where
   toBS = E.encodeUtf8
   fromBS = E.decodeUtf8
+  
+class RedisKey a where
+  (<:>) :: a -> a -> a
 
-(<:>) :: T.Text -> T.Text -> T.Text
-s1 <:> s2 = T.concat [s1, ":", s2]
+instance RedisKey T.Text where
+  s1 <:> s2 = T.concat [s1, ":", s2]
 
-eventIDKeyPrefix :: T.Text
-eventIDKeyPrefix = "event.id"
+newtype EventIDKey = EventIDKey { getEventIDKey :: T.Text } deriving (Eq, Show, BS, RedisKey)
+newtype NextEventIDKey = NextEventIDKey { getNextEventIDKey :: T.Text } deriving (Eq, Show, BS, RedisKey)
 
-nextEventIDKey :: T.Text
-nextEventIDKey = "next.event.id"
+eventIDKeyPrefix :: EventIDKey
+eventIDKeyPrefix = EventIDKey "event.id"
 
-eventIDKey :: T.Text -> T.Text
-eventIDKey nativeEventID = eventIDKeyPrefix <:> nativeEventID
+nextEventIDKey :: NextEventIDKey
+nextEventIDKey = NextEventIDKey "next.event.id"
+
+eventIDKey :: T.Text -> EventIDKey
+eventIDKey nativeEventID = eventIDKeyPrefix <:> (EventIDKey nativeEventID)
 
 getOrSetEventID :: T.Text -> Redis -> IO (Int)
 getOrSetEventID nativeEventID r = getOrSetID (eventIDKey nativeEventID) nextEventIDKey r
 
-artistIDKeyPrefix :: T.Text
-artistIDKeyPrefix = "artist.id"
+-- getOrSetFooID :: T.Text -> T.Text -> Redis -> IO (Int)
+-- getOrSetFooID x y r = getOrSetID x y r
 
-nextArtistIDKey :: T.Text
-nextArtistIDKey = "next.artist.id"
+-- artistIDKeyPrefix :: T.Text
+-- artistIDKeyPrefix = "artist.id"
 
-artistIDKey :: T.Text -> T.Text
-artistIDKey nativeArtistID = artistIDKeyPrefix <:> nativeArtistID
+-- nextArtistIDKey :: T.Text
+-- nextArtistIDKey = "next.artist.id"
 
-getOrSetArtistID :: T.Text -> Redis -> IO (Int)
-getOrSetArtistID nativeArtistID r = getOrSetID (artistIDKey nativeArtistID) nextArtistIDKey r
+-- artistIDKey :: T.Text -> T.Text
+-- artistIDKey nativeArtistID = artistIDKeyPrefix <:> nativeArtistID
 
-venueIDKeyPrefix :: T.Text
-venueIDKeyPrefix = "venue.id"
+-- getOrSetArtistID :: T.Text -> Redis -> IO (Int)
+-- getOrSetArtistID nativeArtistID r = getOrSetID (artistIDKey nativeArtistID) nextArtistIDKey r
 
-nextVenueIDKey :: T.Text
-nextVenueIDKey = "next.venue.id"
+-- venueIDKeyPrefix :: T.Text
+-- venueIDKeyPrefix = "venue.id"
 
-venueIDKey :: T.Text -> T.Text
-venueIDKey nativeVenueID = venueIDKeyPrefix <:> nativeVenueID
+-- nextVenueIDKey :: T.Text
+-- nextVenueIDKey = "next.venue.id"
 
-getOrSetVenueID :: T.Text -> Redis -> IO (Int)
-getOrSetVenueID nativeVenueID r = getOrSetID (venueIDKey nativeVenueID) nextVenueIDKey r
+-- venueIDKey :: T.Text -> T.Text
+-- venueIDKey nativeVenueID = venueIDKeyPrefix <:> nativeVenueID
+
+-- getOrSetVenueID :: T.Text -> Redis -> IO (Int)
+-- getOrSetVenueID nativeVenueID r = getOrSetID (venueIDKey nativeVenueID) nextVenueIDKey r
 
 -- Return an ID for idKey if it exists, otherwise make a new one. This
 -- function is race-free. If two processes call it at the same time,
 -- only one will create a new ID; the other will return the same ID as
 -- the first.
-getOrSetID :: T.Text -> T.Text -> Redis ->IO (Int)
+getOrSetID :: (RedisKey a, RedisKey b, BS a, BS b) => a -> b -> Redis ->IO (Int)
 getOrSetID idKey nextIdKey r = do
   id <- get r idKey >>= fromRBulk
   case id of
