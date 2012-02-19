70299082d35e2d9cda24fb7959a2f5bdb022b70b
Drew Hess <src@drewhess.com>
2012-02-18 21:01:13 -0800

    GetEvent.hs now outputs a key-value pair representation of the event.
    Added this code mostly just to document how it can be done using
    Data.Generics.

--- GetEvent.hs
+++ GetEvent.hs
@@ -4,14 +4,21 @@ import Text.XML.Light
 import Data.Maybe
 import Data.Aeson.Generic (encode)
 import Data.ByteString.Lazy as B
+import Data.Data
+import Data.Generics.Text
 
 eventURL = "http://schedule.sxsw.com/2011/events/event_MS14879"
+
+introspectData :: Data a => a -> [(String, String)]
+introspectData a = Prelude.zip fields $ gmapQ gshow a
+    where fields = constrFields $ toConstr a
 
 main :: IO ()
 main = do
   Just xml <- getEventDoc eventURL
   Prelude.putStrLn $ "Event details for " ++ eventURL ++ ":\n"
   B.putStrLn $ encode $ fromJust $ parseEvent xml
+  Prelude.putStrLn $ show $ introspectData $ fromJust $ parseEvent xml
 
 getEventDoc :: String -> IO (Maybe Element)
 getEventDoc = fmap parseXMLDoc . unsafeCurlGetString
