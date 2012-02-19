db84c894e638675fc4df197a96a9944d71609f9a
Drew Hess <src@drewhess.com>
2012-02-18 21:01:54 -0800

    Print a list of Redis event IDs when adding them to the DB.

--- StoreAllEvents.hs
+++ StoreAllEvents.hs
@@ -10,4 +10,5 @@ main :: IO ()
 main = do
   redis <- connect localhost defaultPort
   eventURLs <- liftM mconcat $ mapM eventURLsForDay [15, 16, 17, 18, 19, 20]
-  mapM_ (\id -> getOrSetEventID (pack id) redis) eventURLs
+  eventIDs <- mapM (\id -> getOrSetEventID (pack id) redis) eventURLs
+  mapM_ (\id -> putStrLn $ show id) eventIDs
