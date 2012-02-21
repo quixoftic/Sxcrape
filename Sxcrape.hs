261718bb8e1a5eb1dc394bb068f895da718c91d1
Drew Hess <src@drewhess.com>
2012-02-20 17:46:53 -0800

    Rename "details" mode to "parse".

--- Sxcrape.hs
+++ Sxcrape.hs
@@ -12,35 +12,35 @@ import Data.ByteString.Lazy as B hiding (map)
 type URL = String
 
 data Sxcrape = Events { day :: [Day] }
-             | Details { urls :: FilePath
-                       , write_to_file :: Bool
-                       , events :: [URL]
-                       }
+             | Parse { urls :: FilePath
+                     , write_to_file :: Bool
+                     , events :: [URL]
+                     }
              deriving (Typeable, Data, Eq, Show)
 
 events_ = Events { day = def &= help "Get a specific day (default is all days)"
                  } &= help "Get music event URLs"
          
-details_ = Details { urls = def &= typFile &= help "read event URLs from a file"
-                   , write_to_file = def &= help "write JSON for each event to a separate file (default is to write them all to stdout)"
-                   , events = def &= args &= typ "URL ..."
-                   } &= help "Get music event details as JSON"
+parse = Parse { urls = def &= typFile &= help "read event URLs from a file"
+              , write_to_file = def &= help "write JSON for each event to a separate file (default is to write them all to stdout)"
+              , events = def &= args &= typ "URL ..."
+              } &= help "Parse music event details into JSON"
          
-mode = cmdArgsMode $ modes [events_, details_] &= help "Scrape the SXSW music schedule" &= program "sxcrape" &= summary "sxcrape 0.1"
+mode = cmdArgsMode $ modes [events_, parse] &= help "Scrape the SXSW music schedule" &= program "sxcrape" &= summary "sxcrape 0.1"
 
 main :: IO ()
 main = cmdArgsRun mode >>= \x -> case x of
   Events {} -> runEvents x
-  Details {} -> runDetails x
+  Parse {} -> runParse x
 
 runEvents :: Sxcrape -> IO ()
 runEvents opts@(Events {}) = if (day opts) == []
                              then eventURLs >>= mapM_ Prelude.putStrLn
                              else mapM eventURLsForDay (day opts) >>= mapM_ Prelude.putStrLn . mconcat
 
-runDetails :: Sxcrape -> IO ()
-runDetails opts@Details {..}
-  | events == [] = Prelude.putStrLn "details mode needs at least one event URL"
+runParse :: Sxcrape -> IO ()
+runParse opts@Parse {..}
+  | events == [] = Prelude.putStrLn "parse mode needs at least one event URL"
   | otherwise   = do
     jsonResults <- mapM eventDetailsAsJson events
     mapM_ B.putStrLn jsonResults
