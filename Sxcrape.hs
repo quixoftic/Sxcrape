dcde3501d2c22cad9dccfaca4a3bdc14595575ab
Drew Hess <src@drewhess.com>
2012-02-21 00:36:53 -0800

    Add "multidump" for dumping >1 event page at a time (to files).

--- Sxcrape.hs
+++ Sxcrape.hs
@@ -7,30 +7,40 @@ import Network.Curl
 import Text.XML.Light
 import Data.Maybe
 import Data.Aeson.Generic (encode)
-import Data.ByteString.Lazy as B hiding (map)
+import Data.ByteString.Lazy as B (putStrLn, ByteString)
+import System.Directory
+import Control.Monad
+import Control.Exception (bracket)
 
 type URL = String
 
 data Sxcrape = Events { day :: [Day] }
              | Dump { event :: URL }
+             | MultiDump { output_dir :: Maybe FilePath,
+                           urls :: FilePath }
              | Parse { events :: [URL] }
              deriving (Typeable, Data, Eq, Show)
 
 events_ = Events { day = def &= help "Get a specific day (default is all days)"
                  } &= help "Get music event URLs"
-         
+
 dump = Dump { event = def &= argPos 0 &= typ "URL"
             } &= help "Download music event HTML"
 
+multiDump = MultiDump { output_dir = def &= typDir &= help "output dump files in this directory"
+                      , urls = def &= argPos 0 &= typFile
+                      } &= help "Download multiple events from a file containing a list of URLs, and write the HTML of each to a separate file"
+
 parse = Parse { events = def &= args &= typ "URL ..."
               } &= help "Parse music event details into JSON"
-         
-mode = cmdArgsMode $ modes [events_, dump, parse] &= help "Scrape the SXSW music schedule" &= program "sxcrape" &= summary "sxcrape 0.1"
+
+mode = cmdArgsMode $ modes [events_, dump, multiDump, parse] &= help "Scrape the SXSW music schedule" &= program "sxcrape" &= summary "sxcrape 0.1"
 
 main :: IO ()
 main = cmdArgsRun mode >>= \x -> case x of
   Events {} -> runEvents x
   Dump {} -> runDump x
+  MultiDump {} -> runMultiDump x
   Parse {} -> runParse x
 
 runEvents :: Sxcrape -> IO ()
@@ -40,6 +50,17 @@ runEvents opts@(Events {}) = if (day opts) == []
 
 runDump :: Sxcrape -> IO ()
 runDump opts@Dump {..} = unsafeCurlGetString event >>= Prelude.putStrLn
+
+runMultiDump :: Sxcrape -> IO ()
+runMultiDump opts@MultiDump {..} = do
+  let outputDir = case output_dir of
+        Just dirName -> dirName
+        Nothing -> "."
+  createDirectoryIfMissing True outputDir
+  eventURLs <- fmap lines $ readFile urls
+  withCurrentDirectory outputDir $ do
+    contents <- mapM unsafeCurlGetString eventURLs
+    mapM_ (\(url, contents) -> writeFile (urlToFilename url) contents) $ zip eventURLs contents
 
 runParse :: Sxcrape -> IO ()
 runParse opts@Parse {..}
@@ -58,3 +79,16 @@ getEventDoc = fmap parseXMLDoc . unsafeCurlGetString
 
 unsafeCurlGetString :: String -> IO String
 unsafeCurlGetString url = curlGetString url [] >>= return . snd
+
+urlToFilename :: URL -> FilePath
+urlToFilename = fromJust . eventFromURL
+
+withCurrentDirectory :: FilePath -> IO a -> IO a
+withCurrentDirectory dirName io =
+  bracket
+      (do
+          cwd <- getCurrentDirectory
+          when (dirName /= "") (setCurrentDirectory dirName)
+          return cwd)
+      (\orig -> setCurrentDirectory orig)
+      (const io)
