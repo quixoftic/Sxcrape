a59df3929a0f7a24a1dbc02ec0df99cf01deb201
Drew Hess <src@drewhess.com>
2012-09-20 18:03:09 -0700

    Add a batchparse command.
    This command can parse one or more event HTML files from the web site,
    or from local files (e.g., created with the dump or batchdump
    command).
    
    Also, changed the uriToFileName function so that it can handle http://
    URLs or file paths: it just strips off the file part of the (HTTP or
    local filesystem) path.

--- src/Sxcrape.hs
+++ src/Sxcrape.hs
@@ -7,7 +7,7 @@ import Network.HTTP.Conduit hiding (def)
 import Data.Maybe
 import qualified Data.Aeson.Generic as Aeson (encode)
 import Data.ByteString.Lazy (ByteString)
-import qualified Data.ByteString.Lazy.Char8 as C8 (putStrLn)
+import qualified Data.ByteString.Lazy.Char8 as C8 (putStrLn, writeFile)
 import System.Directory
 import Control.Monad
 import Control.Exception (bracket)
@@ -17,6 +17,8 @@ import qualified Data.Text.Lazy.Encoding as E
 import Data.Either
 import Paths_Sxcrape (version)
 import Data.Version (showVersion)
+import System.FilePath.Posix as Posix (splitFileName)
+import Network.URI as URI (uriPath, parseURIReference)
 
 type URL = String
 
@@ -25,6 +27,8 @@ data Sxcrape = Events { day :: [Day] }
              | BatchDump { output_dir :: Maybe FilePath,
                            urls :: [URL] }
              | Parse { event :: URL }
+             | BatchParse { output_dir :: Maybe FilePath,
+                            urls :: [URL] }
              deriving (Typeable, Data, Eq, Show)
 
 events_ = record Events { day = def } [ day := def += help "Get a specific day (default is all days)"
@@ -41,7 +45,12 @@ batchDump = record BatchDump { output_dir = def
 parse = record Parse { event = def } [ event := def += argPos 0 += typ "URL|PATH"
                                      ] += help "Parse music event details into JSON, using a URL or the path to a file containing the event HTML"
 
-mode = cmdArgsMode_ $ modes_ [events_, dump, batchDump, parse] += help "Scrape the SXSW music schedule" += program "sxcrape" += summary ("sxcrape " ++ showVersion version)
+batchParse = record BatchParse { output_dir = def
+                               , urls = def } [ output_dir := def += typDir += help "output JSON files in this directory"
+                                              , urls := def += args += typ "-|(URL|PATH ...)"
+                                              ] += help "Parse events from one or more URLs given on the command line, or via stdin, into JSON, and store each in a separate file"
+
+mode = cmdArgsMode_ $ modes_ [events_, dump, batchDump, parse, batchParse] += help "Scrape the SXSW music schedule" += program "sxcrape" += summary ("sxcrape " ++ showVersion version)
 
 main :: IO ()
 main = cmdArgsRun mode >>= \x -> case x of
@@ -51,6 +60,9 @@ main = cmdArgsRun mode >>= \x -> case x of
     args <- readMultipleArgs (urls opts)
     runBatchDump args (output_dir opts)
   opts@Parse {} -> runParse $ T.pack (event opts)
+  opts@BatchParse {} -> do
+    args <- readMultipleArgs (urls opts)
+    runBatchParse args (output_dir opts)
 
 -- Read [String] from command line or stdin, convert to [T.Text]
 readMultipleArgs :: [String] -> IO [T.Text]
@@ -71,10 +83,20 @@ runBatchDump urls maybeDirName = do
   createDirectoryIfMissing True outputDir
   withCurrentDirectory outputDir $ do
     contents <- mapM download urls
-    mapM_ (\(url, contents) -> T.writeFile (urlToFilename url) contents) $ zip urls contents
+    mapM_ (\(url, contents) -> T.writeFile (urlToFileName url) contents) $ zip urls contents
 
 runParse :: T.Text -> IO ()
 runParse event = eventDetailsAsJson event >>= C8.putStrLn
+
+runBatchParse :: [T.Text] -> Maybe FilePath -> IO ()
+runBatchParse urls maybeDirName = do
+  let outputDir = fromMaybe "." maybeDirName
+  createDirectoryIfMissing True outputDir
+  -- Some of the input URLs may be local files; read them relative to
+  -- the program's working directory, not the output directory.
+  contents <- mapM eventDetailsAsJson urls
+  withCurrentDirectory outputDir $ do
+    mapM_ (\(url, contents) -> C8.writeFile ((urlToFileName url) ++ ".json") contents) $ zip urls contents
 
 eventDetailsAsJson :: T.Text -> IO ByteString
 eventDetailsAsJson url = getContents url >>= return . Aeson.encode . parseEvent
@@ -86,8 +108,8 @@ eventDetailsAsJson url = getContents url >>= return . Aeson.encode . parseEvent
 download :: T.Text -> IO T.Text
 download url = simpleHttp (T.unpack url) >>= return . E.decodeUtf8
 
-urlToFilename :: T.Text -> FilePath
-urlToFilename = T.unpack . fromJust . eventFromURL
+urlToFileName :: T.Text -> FilePath
+urlToFileName = snd . Posix.splitFileName . URI.uriPath . fromJust . URI.parseURIReference . T.unpack
 
 withCurrentDirectory :: FilePath -> IO a -> IO a
 withCurrentDirectory dirName io =
