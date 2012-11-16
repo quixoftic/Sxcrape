063eeb686c0d84d0cf42581f99be44fb31bb6c59
Drew Hess <src@drewhess.com>
2012-11-15 18:53:20 -0800

    Simplify Redis schema and Redis.hs implementation.
    * Eliminate numeric IDs; just use the native SXSW IDs. This will save
      the client a round-trip to the database when looking up artists or
      venues from event details.
    
    * Simplify the Redis.hs key implementation; it was clever, but didn't
      add much type-safety, in the end.
    
    * Replace the very schema-specific helper functions with generic,
      "unsafe" wrappers around the hedis functions.

--- src/Redis.hs
+++ src/Redis.hs
@@ -10,14 +10,7 @@
 -- Experimental Redis interface for parsed SXSW music event info.
 --
 
-module Redis ( importEventDetails,
-               getOrSetEventID
-             , getOrSetArtistID 
-             , getOrSetVenueID
-             , saddEvents
-             , saddArtists
-             , saddVenues
-             ) where
+module Redis (importEventDetails) where
 
 import qualified ParseEventDoc
 import Event
@@ -27,160 +20,65 @@ import Database.Redis
 import Data.Maybe
 import Control.Monad
 import qualified Data.Aeson.Encode as Aeson (encode)
+import qualified Data.Aeson.Types as Aeson (ToJSON)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Encoding as E
 
--- Import the event details.
+toJSON' :: (Aeson.ToJSON a) => a -> BS.ByteString
+toJSON' = BS.concat . BL.toChunks . Aeson.encode
+
+-- Redis keys
+--
+(<:>) :: T.Text -> T.Text -> BS.ByteString
+s1 <:> s2 = toBS $ T.concat [s1, ":", s2]
+
+toEventKey :: T.Text -> BS.ByteString
+toEventKey = (<:>) "event"
+
+toArtistKey :: T.Text -> BS.ByteString
+toArtistKey = (<:>) "artist"
+
+toVenueKey :: T.Text -> BS.ByteString
+toVenueKey = (<:>) "venue"
+
+eventSetKey :: BS.ByteString
+eventSetKey = "events"
+
+artistSetKey :: BS.ByteString
+artistSetKey = "artists"
+
+venueSetKey :: BS.ByteString
+venueSetKey = "venues"
+
+-- Import functions.
+--
 importEventDetails :: (Event, Artist, Venue) -> Redis ()
 importEventDetails (event, artist, venue) =
   let artistName = Artist.name artist
       venueName = Venue.name venue
       eventURL = Event.url event
   in do
-    eventID <- getOrSetEventID eventURL
-    setEventJSON (eventJSONKey $ fromBS eventID) event
-    saddEvents eventURL
-    saddArtists artistName
-    saddVenues venueName
+    unsafeSet (toEventKey eventURL) (toJSON' event)
+    unsafeSet (toArtistKey artistName) (toJSON' artist)
+    unsafeSet (toVenueKey venueName) (toJSON' venue)
+    unsafeSadd eventSetKey (toBS eventURL)
+    unsafeSadd artistSetKey (toBS artistName)
+    unsafeSadd venueSetKey (toBS venueName)
     return ()
-  
-getOrSetEventID :: T.Text -> Redis (BS.ByteString)
-getOrSetEventID nativeEventID = getOrSetID (eventIDKey nativeEventID) nextEventIDKey
 
-getOrSetArtistID :: T.Text -> Redis (BS.ByteString)
-getOrSetArtistID nativeArtistID = getOrSetID (artistIDKey nativeArtistID) nextArtistIDKey
-
-getOrSetVenueID :: T.Text -> Redis (BS.ByteString)
-getOrSetVenueID nativeVenueID = getOrSetID (venueIDKey nativeVenueID) nextVenueIDKey
-
-saddEvents :: T.Text -> Redis (Integer)
-saddEvents nativeEventID = saddNativeID eventSetKey nativeEventID
-
-saddArtists :: T.Text -> Redis (Integer)
-saddArtists nativeArtistID = saddNativeID artistSetKey nativeArtistID
-
-saddVenues :: T.Text -> Redis (Integer)
-saddVenues nativeVenueID = saddNativeID venueSetKey nativeVenueID
-
-type Key = T.Text
-
-data IDKey a k = IDKey a k deriving (Eq, Show)
-data NextIDKey a k = NextIDKey a k deriving (Eq, Show)
-data SetKey a k = SetKey a k deriving (Eq, Show)
-data JSONKey a k = JSONKey a k deriving (Eq, Show)
-
--- Build keys from namespaces with this operator.
-(<:>) :: T.Text -> T.Text -> T.Text
-s1 <:> s2 = T.concat [s1, ":", s2]
-
--- Event keys.
+-- Convenience wrappers around Redis functions.
 --
-data EventT = EventT deriving (Eq, Show)
-type EventIDKey = IDKey EventT Key
-type NextEventIDKey = NextIDKey EventT Key
-type EventSetKey = SetKey EventT Key
-type EventJSONKey = JSONKey EventT Key
+unsafeSet :: BS.ByteString -> BS.ByteString -> Redis (Status)
+unsafeSet key value = do
+  Right status <- set key value
+  return status
 
-nextEventIDKey :: NextEventIDKey
-nextEventIDKey = NextIDKey EventT "next.event.id"
-
-eventIDKeyPrefix :: T.Text
-eventIDKeyPrefix = "event.id"
-
-eventIDKey :: T.Text -> EventIDKey
-eventIDKey nativeEventID = IDKey EventT $ eventIDKeyPrefix <:> nativeEventID
-
-eventSetKey :: EventSetKey
-eventSetKey = SetKey EventT "events"
-
-eventJSONKeyPrefix :: T.Text
-eventJSONKeyPrefix = "event"
-
-eventJSONKey :: T.Text -> EventJSONKey
-eventJSONKey nativeEventID = JSONKey EventT $ eventJSONKeyPrefix <:> nativeEventID
-
--- Artist keys.
---
-data ArtistT = ArtistT deriving (Eq, Show)
-type ArtistIDKey = IDKey ArtistT Key
-type NextArtistIDKey = NextIDKey ArtistT Key
-type ArtistSetKey = SetKey ArtistT Key
-
-artistIDKeyPrefix :: T.Text
-artistIDKeyPrefix = "artist.id"
-
-nextArtistIDKey :: NextArtistIDKey
-nextArtistIDKey = NextIDKey ArtistT "next.artist.id"
-
-artistIDKey :: T.Text -> ArtistIDKey
-artistIDKey nativeArtistID = IDKey ArtistT  $ artistIDKeyPrefix <:> nativeArtistID
-
-artistSetKey :: ArtistSetKey
-artistSetKey = SetKey ArtistT "artists"
-
--- Venue keys.
---
-data VenueT = VenueT deriving (Eq, Show)
-type VenueIDKey = IDKey VenueT Key
-type NextVenueIDKey = NextIDKey VenueT Key
-type VenueSetKey = SetKey VenueT Key
-
-venueIDKeyPrefix :: T.Text
-venueIDKeyPrefix = "venue.id"
-
-nextVenueIDKey :: NextVenueIDKey
-nextVenueIDKey = NextIDKey VenueT "next.venue.id"
-
-venueIDKey :: T.Text -> VenueIDKey
-venueIDKey nativeVenueID = IDKey VenueT $ venueIDKeyPrefix <:> nativeVenueID
-
-venueSetKey :: VenueSetKey
-venueSetKey = SetKey VenueT "venues"
-
-
--- Generic functions.
---
-
--- Return an ID for idKey if it exists, otherwise make a new one by
--- incrementing the next ID key. This function is race-free. If two
--- processes call it at the same time, only one will create a new ID;
--- the other will return the same ID as the first.
---
-getOrSetID :: IDKey a Key -> NextIDKey a Key -> Redis (BS.ByteString)
-getOrSetID (IDKey _ idKey) (NextIDKey _ nextIDKey) =
-  let key = toBS idKey
-      nextkey = toBS nextIDKey
-    in do
-    Right maybeID <- get key
-    case maybeID of
-      Just id -> return id
-      Nothing -> do
-        Right newID <- incr nextkey
-        Right reply <- setnx key $ integerToValue newID
-        if reply
-          then return $ integerToValue newID
-          else do Right (Just id) <- get key
-                  return id
-
-setEventJSON :: EventJSONKey -> Event -> Redis (Status)
-setEventJSON (JSONKey _ eventKey) event =
-  let key = toBS eventKey
-      value = BS.concat $ BL.toChunks $ Aeson.encode event
-  in do
-    Right status <- set key value
-    return status
-
--- Type-safe function for adding native (SXSW) IDs to a set.
---
-saddNativeID :: SetKey a Key -> T.Text -> Redis (Integer)
-saddNativeID (SetKey _ key) nativeID = 
-  let k = toBS key
-      id = toBS nativeID
-  in do
-    Right result <- sadd k [id]
-    return result
+unsafeSadd :: BS.ByteString -> BS.ByteString -> Redis (Integer)
+unsafeSadd key value = do
+  Right result <- sadd key [value]
+  return result
 
 -- Helpers
 --
@@ -195,9 +93,3 @@ toBS = E.encodeUtf8 . T.toStrict
 
 fromBS :: BS.ByteString -> T.Text
 fromBS = T.fromStrict . E.decodeUtf8
-
-
--- Canary test for key type-safety. This should not compile:
---
---getOrSetFooID :: T.Text -> T.Text -> Redis (BS.ByteString)
---getOrSetFooID x y = getOrSetID x y
